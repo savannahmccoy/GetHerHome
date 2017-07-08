f0fd8e84cc22c30c3f55224ef478697a2b0a06f9
Savannah McCoy <savannahmccoy@Savannahs-MacBook-Pro.local>
2017-07-08 16:48:19 -0700

    Initial Storyboard Setup 40% complete

---
+++ GetHerHome/CreateAccountViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CreateAccountViewController.swift
+//  GetHerHome
+//
+//  Created by Savannah McCoy on 7/8/17.
+//  Copyright © 2017 Savannah McCoy. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
